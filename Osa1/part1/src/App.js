91f52577cba0e8986e952a988282a8d6e38c82b2
vainlmzz <vainlmzz@student.jyu.fi>
2021-07-14 16:21:31 +0300

    src lisätty

---
+++ Osa1/part1/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <div>
+      <h1> Greetings </h1>
+      <Hello name="Maya" age={26 +10} />
+      <Hello name={nimi} age={ika}/>
+    </div>
+  )
+} 
+
+export default App
