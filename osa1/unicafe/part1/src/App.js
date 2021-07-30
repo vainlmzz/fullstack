31f3fca9f79fc1c6838711f87d6539c42d99d29a
vainlmzz <vainlmzz@student.jyu.fi>
2021-07-30 17:49:48 +0300

    1.6

--- osa1/unicafe/part1/src/App.js
+++ osa1/unicafe/part1/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {props.name}
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Display = (props) => {
+  return (
+    <div>{props.name} {props.lkm}</div>
+  )
+}
+
+
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header name={"give feedback"} />
+      <button onClick={() => setGood(good + 1)}> 
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}> 
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}> 
+        bad
+      </button>
+      <Header name={"statistics"} />
+      <Display name={"good"} lkm={good}/>
+      <Display name={"neutral"} lkm={neutral}/>
+      <Display name={"bad"} lkm={bad}/>
+    </div>
+    )
+  }
+  
+    export default App
