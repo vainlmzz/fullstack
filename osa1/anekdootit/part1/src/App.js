7c703d37b36aa7861f1579d42c678348ca1e4bf7
vainlmzz <vainlmzz@student.jyu.fi>
2021-08-03 14:46:26 +0300

    1.13*

--- osa1/anekdootit/part1/src/App.js
+++ osa1/anekdootit/part1/src/App.js
@@ -11,6 +11,8 @@ const Button = (props) => (
 function ranNum(min, max) {
   return Math.floor(min + Math.random()*(max + 1 - min))
 }
+
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -20,9 +22,16 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
-  ]
-   
+    ]
+  
+
+  
+  
   const [selected, setSelected] = useState(0)
+  const [points, setVote] = useState([0,0,0,0,0,0,0])
+
+  
+  
 
   const setSel = (value) => {
     return (
@@ -30,14 +39,28 @@ const App = () => {
     )
   }
 
+  const setVo = () => {
+    const copy = {...points}
+    return (
+      copy[selected] += 1,
+      setVote(copy),
+      console.log(points)
+      //console.log(copy)
+    )
+    
+  }
+
   return (
     <div>
+      
       <p>{anecdotes[selected]}</p>
-
+      <p> has {points[selected]} votes</p>
+      <Button handleClick={() => setVo()} text={"vote"}/> 
       <Button handleClick={() => setSel(ranNum(0,anecdotes.length-1))} text={"next anecdote"}/> 
       
     </div>
   )
 }
 
+// <h1> Anecdote of the day</h1> <h1> Anecdote with most votes</h1>
 export default App
