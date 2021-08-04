2c828eee3d1d59bf8d6f94cc339e3c6461bfd2ac
vainlmzz <vainlmzz@student.jyu.fi>
2021-08-04 15:29:44 +0300

    2.1

--- osa2/part1/src/App.js
+++ osa2/part1/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+const Header = ( {course} ) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {course.name}
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Course = ( {course} ) => {
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+    </div>
+  )
+}
+
+
+//Sovelluksen täytyy nyt toimia riippumatta kurssissa olevien osien määrästä. Eli varmista, että 
+//sovellus toimii jos lisäät tai poistat kurssin osia.
+
+//Varmista myös, että konsolissa ei näy mitään virheilmoituksia!
+
+
+
+const Content = ( {course} ) => {
+  return (
+      <div>
+        {course.parts.map(part => <ul key={part.id}><Part part={part}/></ul>)}
+      </div>
+  )
+}    
+
+
+// tässä vaan osan nimi ja teht.määrä
+const Part = ( {part} ) => {
+  return (
+    <div>
+      {part.name} {part.exercises}
+    </div>
+  )
+}
+
+
+  
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+  export default App
