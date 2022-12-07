b7f34e019f358b239646e787f6c00f6f0cf33b14
vainlmzz <vainlmzz@student.jyu.fi>
2022-12-07 18:35:36 +0200

    4.4

--- osa4/blogilista2/utils/list_helper.js
+++ osa4/blogilista2/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
   }
 
+const totalLikes = (blogs) => {
+return  blogs.length === 0
+?   0
+:   blogs.map(blog => blog.likes).reduce((edel, seur) => edel + seur);
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
