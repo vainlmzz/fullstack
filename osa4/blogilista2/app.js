c6f78ee409c4bc0bef9056388eae21859ff3b04f
vainlmzz <vainlmzz@student.jyu.fi>
2022-12-28 14:04:52 +0200

    4.18

--- osa4/blogilista2/app.js
+++ osa4/blogilista2/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 
@@ -16,7 +17,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-
+app.use('/api/login', loginRouter)
 
 
 
